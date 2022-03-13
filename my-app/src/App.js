5ee6ade33f39f670f52c5b60c7c33cb176378a25
www.hicham2005@gmail.com <www.hicham2005@gmail.com>
2022-03-13 19:20:14 +0100

    dydyyx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+
 function App() {
+  
+  let [ Name, setName ] = useState(0); 
+  
+  const clickHandeler = ()=>{
+    setName(Name+1);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{ Name }</h1>
+      <button onClick={ clickHandeler }>Click me</button>
     </div>
   );
 }
